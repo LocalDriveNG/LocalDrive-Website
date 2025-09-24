96d2d01a4ead11c0b008d01ab42e65641c947b61
khennyyb <khennyphresh@gmail.com>
2025-09-24 15:37:39 +0100

    fixed logo height

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -50,7 +50,7 @@ const Header = () => {
               <img 
                 src={logoFull} 
                 alt="LocalDrive - Your local driving way, reimagined"
-                className="h-12 w-auto max-w-[5000px] font-bold filter contrast-125 hover:scale-105 transition-transform duration-200"
+                className="h-24 w-auto max-w-[320px] font-bold filter contrast-125 hover:scale-105 transition-transform duration-200"
               />
             </Link>
           </div>
