a8f2e2b57d174427b097e65f378f36d174b7bb8b
khennyyb <khennyphresh@gmail.com>
2025-09-27 10:59:38 +0100

    added sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig(({ mode }) => ({
   server: {
@@ -16,6 +17,13 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+
+      plugins: [
+    sitemap({
+      hostname: 'https://localdriveapp.com',
+      outDir: 'dist',  // where vite build outputs
+    }),
+  ],
   },
   build: {
     rollupOptions: {
