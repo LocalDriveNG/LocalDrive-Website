b80745b55e747082dc99597fc814ece6eca5f8cb
khennyyb <khennyphresh@gmail.com>
2025-10-01 17:29:48 +0100

    fix view issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger"
 import sitemap from "vite-plugin-sitemap"
 
 export default defineConfig(({ mode }) => ({
+  base: "./",
   server: {
     host: "::",
     port: 8080,
